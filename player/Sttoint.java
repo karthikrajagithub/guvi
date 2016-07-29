64ade483b061a310de8117a70c16306037c6e803
karthikrajagithub <karthikraja95it@gmail.com>
2016-07-29 12:43:13 +0530

    Add files via upload

---
+++ player/Sttoint.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Sttoint
+{
+public static void main(String ar[])
+{
+String st;
+Scanner sc=new Scanner(System.in);
+st=sc.next();
+int num=Integer.parseInt(st);
+System.out.println(num);
+}
+}
